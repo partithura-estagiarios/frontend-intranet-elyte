cfe8ca2f1d79a568ee169748ca5384db0e777e70
Juliano Stroschon <julianostroschon@gmail.com>
2023-01-19 10:04:14 -0300

    🔥 removidas rotas desnecessárias

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,13 +1,8 @@
 <template>
   <div class="title">
     <h1>{{ $t("about") }}</h1>
-    <q-input v-model="name" type="text" filled class="bg-grey" />
-    <q-btn :to="`/${name}/`">Submit</q-btn>
   </div>
 </template>
-<script setup lang="ts">
-const name = ref("");
-</script>
 
 <style scoped>
 .title {

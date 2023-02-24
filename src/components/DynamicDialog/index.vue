619c70372cf37d07a621b3e50414f4c69ba0561a
gabriel_gras <gabrielgras10@gmail.com>
2023-02-24 10:36:47 -0300

    💄 adicionado componente dynamic dialog

---
+++ src/components/DynamicDialog/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-dialog v-model="modal" persistent>
+    <MainCard class="background-dark" v-bind="$props">
+      <slot />
+    </MainCard>
+  </q-dialog>
+</template>
+
+<script setup lang="ts">
+const modal = ref(false);
+
+watchEffect(() => {
+  modal.value = props.open;
+});
+
+const props = defineProps({
+  open: Boolean,
+  title: String,
+});
+</script>

17c8983c9217da1cc8a8d54e7ce14c0eae361d28
gabriel_gras <gabrielgras10@gmail.com>
2023-02-27 14:50:02 -0300

    ♿️ textos corrigidos e organizados

--- locales/br/notifications.ts
+++ locales/br/notifications.ts
@@ -1,14 +1,17 @@
 export default {
-  successLogin: "Você foi logado com sucesso",
-  failLogin: "Houve um erro ao tentar efetuar seu login",
-  sucessSendEmail: "Um link de recuperação foi enviado ao seu e-mail",
-  failSendEmail: "Houve um erro ao tentar enviar o email",
-  failChangePwd: "Houve um erro ao tentar mudar sua senha",
-  sucessChangePwd: "Sua senha foi alterada com sucesso",
-  failDelete: "Houve erro tentando deletar o item",
-  sucessDelete: "O item foi deletado com sucesso",
-  failRegister: "Houve um erro ao criar seu cadastro",
-  sucessRegister: "Seu cadastro foi criado com sucesso",
-  sucessCreateRamal: "Ramal criado com sucesso",
-  failCreateRamal: "Houve um erro ao criar um ramal",
+  success: {
+    login: "Acesso liberado",
+    sendEmail: "Um link de recuperação foi enviado ao seu e-mail",
+    changePwd: "Sua senha foi alterada com sucesso",
+    register: "Seu cadastro foi criado com sucesso",
+    createRamal: "Ramal criado com sucesso",
+  },
+
+  fail: {
+    login: "Houve um erro ao tentar acessar o sistema",
+    sendEmail: "Houve um erro ao tentar enviar o email",
+    changePwd: "Houve um erro ao tentar mudar sua senha",
+    register: "Houve um erro ao criar seu cadastro",
+    createRamal: "Houve um erro ao criar o ramal",
+  },
 };

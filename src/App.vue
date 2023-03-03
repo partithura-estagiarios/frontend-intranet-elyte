874c9166a5f6ea7ee56fe4986359625d96476cfb
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2023-03-03 11:06:51 -0300

    ✨ Persistencia dados local

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ useHead({
 </script>
 
 <template>
-  <HeaderBar class="margin" v-if="$route.fullPath != '/login'" />
+  <HeaderBar class="margin" v-if="!$route.fullPath.includes('/login')" />
   <RouterView />
 </template>
 

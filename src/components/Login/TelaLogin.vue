4ec9b48b83f2d68772eff8e4276b9569453cf825
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2023-03-07 08:35:40 -0300

    💄 Remoção inline style do fundo padrão

--- src/components/Login/TelaLogin.vue
+++ src/components/Login/TelaLogin.vue
@@ -2,7 +2,7 @@
   <q-img
     class="background-image"
     src="../../assets/loginImages/fundoLogin.png"
-    style="height: 100%; max-width: 100%"
+    height="100%"
     no-transition
     no-spinner
   >

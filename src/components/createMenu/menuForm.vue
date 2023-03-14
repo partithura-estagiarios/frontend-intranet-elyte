ec99bd64ab20eb19d8c5d7beff8f35a2412c48e0
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2023-03-14 09:56:10 -0300

    💄 Atualização tela de cadastro de cardápio

--- src/components/createMenu/menuForm.vue
+++ src/components/createMenu/menuForm.vue
@@ -1,8 +1,32 @@
 <template>
-  <q-form>
+  <div class="items-center column q-mt-xl" style="max-width: 1800px">
     <q-input
-      v-model="menu.menu_complement"
+      v-model="menu.salad"
+      :label="$t('text.menu.salad')"
+      class="tamanho q-mx-xl"
+      filled
+      stack-label
+      lazy-rules
+      :rules="[
+        (val) => (val && val.length > 0) || $t('warning.menuWarning.salad'),
+      ]"
+    />
+    <q-input
+      v-model="menu.rice"
+      :label="$t('text.menu.rice')"
+      class="tamanho q-mx-xl"
+      filled
+      stack-label
+      lazy-rules
+      :rules="[
+        (val) => (val && val.length > 0) || $t('warning.menuWarning.rice'),
+      ]"
+    />
+    <q-input
+      v-model="menu.complement"
       :label="$t('text.menu.complement')"
+      class="tamanho q-mx-xl"
+      filled
       stack-label
       lazy-rules
       :rules="[
@@ -11,35 +35,10 @@
       ]"
     />
     <q-input
-      v-model="menu.menu_rice"
-      :label="$t('text.menu.rice')"
-      stack-label
-      lazy-rules
-      :rules="[
-        (val) => (val && val.length > 0) || $t('warning.menuWarning.rice'),
-      ]"
-    />
-    <q-input
-      v-model="menu.menu_salad"
-      :label="$t('text.menu.salad')"
-      stack-label
-      lazy-rules
-      :rules="[
-        (val) => (val && val.length > 0) || $t('warning.menuWarning.salad'),
-      ]"
-    />
-    <q-input
-      v-model="menu.menu_protein"
-      :label="$t('text.menu.protein')"
-      stack-label
-      lazy-rules
-      :rules="[
-        (val) => (val && val.length > 0) || $t('warning.menuWarning.protein'),
-      ]"
-    />
-    <q-input
-      v-model="menu.menu_soup"
+      v-model="menu.soup"
       :label="$t('text.menu.soup')"
+      class="tamanho q-mx-xl"
+      filled
       stack-label
       lazy-rules
       :rules="[
@@ -47,8 +46,33 @@
       ]"
     />
     <q-input
-      v-model="menu.menu_day"
+      v-model="menu.protein"
+      :label="$t('text.menu.protein')"
+      class="tamanho q-mx-xl"
+      filled
+      stack-label
+      lazy-rules
+      :rules="[
+        (val) => (val && val.length > 0) || $t('warning.menuWarning.protein'),
+      ]"
+    />
+    <q-input
+      v-model="menu.dessert"
+      :label="$t('text.menu.dessert')"
+      class="tamanho q-mx-xl"
+      filled
+      stack-label
+      lazy-rules
+      :rules="[
+        (val) => (val && val.length > 0) || $t('warning.menuWarning.dessert'),
+      ]"
+    />
+    <q-input
+      v-model="menu.day"
       :label="$t('text.day')"
+      class="tamanho q-mx-xl"
+      type="date"
+      filled
       stack-label
       lazy-rules
       :rules="[
@@ -56,42 +80,36 @@
       ]"
     />
     <q-input
-      v-model="menu.menu_week"
+      v-model="menu.week"
       :label="$t('text.week')"
+      class="tamanho q-mx-xl"
+      filled
       stack-label
       lazy-rules
       :rules="[
         (val) => (val && val.length > 0) || $t('warning.menuWarning.week'),
       ]"
     />
-    <q-card-actions align="right">
-      <q-btn
-        flat
-        :label="$t('action.confirm.index')"
-        color="primary"
-        @click="emit('confirm', menu)"
-      />
-      <q-btn
-        flat
-        :label="$t('action.cancel.index')"
-        color="primary"
-        @click="emit('cancel')"
-      />
-    </q-card-actions>
-  </q-form>
+  </div>
 </template>
 
 <script setup lang="ts">
 const menu = reactive({
-  menu_complement: "",
-  menu_dessert: "",
-  menu_protein: "",
-  menu_salad: "",
-  menu_rice: "",
-  menu_soup: "",
-  menu_week: "",
-  menu_day: "",
+  complement: "",
+  dessert: "",
+  protein: "",
+  salad: "",
+  rice: "",
+  soup: "",
+  week: "",
+  day: "",
 });
 
-const emit = defineEmits(["cancel", "confirm"]);
+const emit = defineEmits(["confirm"]);
 </script>
+
+<style scoped>
+.tamanho {
+  width: 40rem;
+}
+</style>

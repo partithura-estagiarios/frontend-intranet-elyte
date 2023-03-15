fede71fce73ac0d1440636e2d646cb01b4186544
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2023-03-15 09:56:33 -0300

    💄 Ajuste no q-select para as options serem visiveis

--- src/components/createMenu/menuForm.vue
+++ src/components/createMenu/menuForm.vue
@@ -1,99 +1,95 @@
 <template>
-  <q-form class="items-center column">
-    <q-input
-      v-model="menu.salad"
-      :label="$t('text.menu.salad')"
-      class="tamanho"
-      filled
-      stack-label
-      lazy-rules
-      :rules="[
-        (val) => (val && val.length > 0) || $t('warning.menuWarning.salad'),
-      ]"
-    />
-    <q-input
-      v-model="menu.rice"
-      :label="$t('text.menu.rice')"
-      class="tamanho"
-      filled
-      stack-label
-      lazy-rules
-      :rules="[
-        (val) => (val && val.length > 0) || $t('warning.menuWarning.rice'),
-      ]"
-    />
-    <q-input
-      v-model="menu.complement"
-      :label="$t('text.menu.complement')"
-      class="tamanho"
-      filled
-      stack-label
-      lazy-rules
-      :rules="[
-        (val) =>
-          (val && val.length > 0) || $t('warning.menuWarning.complement'),
-      ]"
-    />
-    <q-input
-      v-model="menu.soup"
-      :label="$t('text.menu.soup')"
-      class="tamanho"
-      filled
-      stack-label
-      lazy-rules
-      :rules="[
-        (val) => (val && val.length > 0) || $t('warning.menuWarning.soup'),
-      ]"
-    />
-    <q-input
-      v-model="menu.protein"
-      :label="$t('text.menu.protein')"
-      class="tamanho"
-      filled
-      stack-label
-      lazy-rules
-      :rules="[
-        (val) => (val && val.length > 0) || $t('warning.menuWarning.protein'),
-      ]"
-    />
-    <q-input
-      v-model="menu.dessert"
-      :label="$t('text.menu.dessert')"
-      class="tamanho"
-      filled
-      stack-label
-      lazy-rules
-      :rules="[
-        (val) => (val && val.length > 0) || $t('warning.menuWarning.dessert'),
-      ]"
-    />
-    <q-select
-      v-model="menu.day"
-      :options="options"
-      filled
-      label="Dia da semana"
-      options-dark
-    />
-    <q-input
-      v-model="menu.week"
-      :label="$t('text.week')"
-      class="tamanho"
-      filled
-      stack-label
-      lazy-rules
-      :rules="[
-        (val) => (val && val.length > 0) || $t('warning.menuWarning.week'),
-      ]"
-    />
-    <q-card-actions>
-      <q-btn
-        flat
-        :label="$t('action.confirm.index')"
-        color="primary"
-        @click="emit('confirm', menu)"
+  <div class="row justify-center">
+    <q-form class="col-6">
+      <q-input
+        v-model="menu.salad"
+        :label="$t('text.menu.salad')"
+        filled
+        stack-label
+        lazy-rules
+        :rules="[
+          (val) => (val && val.length > 0) || $t('warning.menuWarning.salad'),
+        ]"
       />
-    </q-card-actions>
-  </q-form>
+      <q-input
+        v-model="menu.rice"
+        :label="$t('text.menu.rice')"
+        filled
+        stack-label
+        lazy-rules
+        :rules="[
+          (val) => (val && val.length > 0) || $t('warning.menuWarning.rice'),
+        ]"
+      />
+      <q-input
+        v-model="menu.complement"
+        :label="$t('text.menu.complement')"
+        filled
+        stack-label
+        lazy-rules
+        :rules="[
+          (val) =>
+            (val && val.length > 0) || $t('warning.menuWarning.complement'),
+        ]"
+      />
+      <q-input
+        v-model="menu.soup"
+        :label="$t('text.menu.soup')"
+        filled
+        stack-label
+        lazy-rules
+        :rules="[
+          (val) => (val && val.length > 0) || $t('warning.menuWarning.soup'),
+        ]"
+      />
+      <q-input
+        v-model="menu.protein"
+        :label="$t('text.menu.protein')"
+        filled
+        stack-label
+        lazy-rules
+        :rules="[
+          (val) => (val && val.length > 0) || $t('warning.menuWarning.protein'),
+        ]"
+      />
+      <q-input
+        v-model="menu.dessert"
+        :label="$t('text.menu.dessert')"
+        filled
+        stack-label
+        lazy-rules
+        :rules="[
+          (val) => (val && val.length > 0) || $t('warning.menuWarning.dessert'),
+        ]"
+      />
+      <q-select
+        v-model="menu.day"
+        :options="options"
+        filled
+        label="Dia da semana"
+        class="q-mb-md"
+        :popup-content-style="{ color: 'black' }"
+      />
+      <q-input
+        v-model="menu.week"
+        :label="$t('text.week')"
+        filled
+        stack-label
+        lazy-rules
+        :rules="[
+          (val) => (val && val.length > 0) || $t('warning.menuWarning.week'),
+        ]"
+      />
+      <q-card-actions>
+        <q-btn
+          flat
+          :label="$t('action.confirm.index')"
+          color="primary"
+          @click="emit('confirm', menu)"
+        />
+      </q-card-actions>
+    </q-form>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -122,7 +118,7 @@ const emit = defineEmits(["confirm"]);
 </script>
 
 <style scoped>
-.tamanho {
-  width: 40rem;
+.extra {
+  color: blue !important;
 }
 </style>

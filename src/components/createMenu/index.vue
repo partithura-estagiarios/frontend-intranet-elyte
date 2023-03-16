31a75cc911eefa1495e848e4b50d2fce06e55b1b
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2023-03-16 14:35:13 -0300

    🔥 Remoção arquivo menuForm.vue

--- src/components/createMenu/index.vue
+++ src/components/createMenu/index.vue
@@ -1,24 +1,136 @@
 <template>
   <span class="text-black text-h4">{{ $t("titles.menu") }}</span>
 
-  <MenuForm class="q-mt-xl" v-bind="$attrs" @confirm="addMenu" />
+  <div class="row justify-center">
+    <Form @submit="onSubmit" :validation-schema="schema" class="col-6">
+      <Field name="day" v-slot="{ errorMessage, value, field }">
+        <q-select
+          :model-value="value"
+          v-bind="field"
+          :label="$t('text.day')"
+          bg-color="grey-3"
+          :options="options"
+          :popup-content-style="{ color: 'black' }"
+          :error-message="errorMessage"
+          :error="!!errorMessage"
+        />
+      </Field>
+
+      <Field name="week" v-slot="{ errorMessage, value, field }">
+        <q-input
+          :model-value="value"
+          v-bind="field"
+          :label="$t('text.week')"
+          filled
+          :error-message="errorMessage"
+          :error="!!errorMessage"
+        />
+      </Field>
+
+      <Field name="salad" v-slot="{ errorMessage, value, field }">
+        <q-input
+          :model-value="value"
+          v-bind="field"
+          :label="$t('text.menu.salad')"
+          filled
+          :error-message="errorMessage"
+          :error="!!errorMessage"
+        />
+      </Field>
+
+      <Field name="rice" v-slot="{ errorMessage, value, field }">
+        <q-input
+          :model-value="value"
+          v-bind="field"
+          :label="$t('text.menu.rice')"
+          filled
+          :error-message="errorMessage"
+          :error="!!errorMessage"
+        />
+      </Field>
+
+      <Field name="protein" v-slot="{ errorMessage, value, field }">
+        <q-input
+          :model-value="value"
+          v-bind="field"
+          :label="$t('text.menu.protein')"
+          filled
+          :error-message="errorMessage"
+          :error="!!errorMessage"
+        />
+      </Field>
+
+      <Field name="complement" v-slot="{ errorMessage, value, field }">
+        <q-input
+          :model-value="value"
+          v-bind="field"
+          :label="$t('text.menu.complement')"
+          filled
+          :error-message="errorMessage"
+          :error="!!errorMessage"
+        />
+      </Field>
+
+      <Field name="soup" v-slot="{ errorMessage, value, field }">
+        <q-input
+          :model-value="value"
+          v-bind="field"
+          :label="$t('text.menu.soup')"
+          filled
+          :error-message="errorMessage"
+          :error="!!errorMessage"
+        />
+      </Field>
+
+      <Field name="dessert" v-slot="{ errorMessage, value, field }">
+        <q-input
+          :model-value="value"
+          v-bind="field"
+          :label="$t('text.menu.dessert')"
+          filled
+          :error-message="errorMessage"
+          :error="!!errorMessage"
+        />
+      </Field>
+      <q-card-actions>
+        <q-btn
+          flat
+          :label="$t('action.confirm.index')"
+          color="primary"
+          type="submit"
+        />
+      </q-card-actions>
+    </Form>
+  </div>
 </template>
 
 <script setup lang="ts">
 import GetMenu from "../../graphql/menu/GetMenu.gql";
 import CreateMenu from "../../graphql/menu/CreateMenu.gql";
 import { Menu } from "../../entities";
+import { Field, Form } from "vee-validate";
+import * as yup from "yup";
 
-function clearForm(formValue: Record<string, string | number>): void {
-  formValue.salad = " ";
-  formValue.rice = " ";
-  formValue.complement = " ";
-  formValue.soup = " ";
-  formValue.protein = " ";
-  formValue.dessert = " ";
-  formValue.day = " ";
-  formValue.week = " ";
-}
+const options = [
+  "Domingo",
+  "Segunda-feira",
+  "Terça-feira",
+  "Quarta-feira",
+  "Quinta-feira",
+  "Sexta-feira",
+  "Sábado",
+];
+
+const schema = yup.object({
+  salad: yup.string().required().label("Salada"),
+  rice: yup.string().required().label("Arroz"),
+  complement: yup.string().required().label("Complemento"),
+  soup: yup.string().required().label("Sopa"),
+  protein: yup.string().required().label("Proteína"),
+  dessert: yup.string().required().label("Sobremesa"),
+  day: yup.string().required().label("Dia"),
+  week: yup.string().required().label("Semana"),
+});
 
 async function addMenu(menu: Record<string, string | number>) {
   try {
@@ -28,8 +140,13 @@ async function addMenu(menu: Record<string, string | number>) {
     positiveNotify(t("notifications.success.createMenu"));
   } catch {
     negativeNotify(t("notifications.fail.createMenu"));
-  } finally {
-    clearForm(menu);
   }
+}
+
+function onSubmit(values: Record<string, string | number>, actions: any) {
+  loga(actions);
+  loga(values);
+  actions.resetForm();
+  addMenu(values, actions);
 }
 </script>

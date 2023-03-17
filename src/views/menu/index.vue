c994a6e4bbe870a28d2b78ef71941c9eabbed21b
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2023-03-17 11:40:48 -0300

    🔥 Parou de importar na rota

--- src/views/menu/index.vue
+++ src/views/menu/index.vue
@@ -1,13 +1,3 @@
 <template>
   <MenuTable />
 </template>
-
-<script setup lang="ts">
-import GetMenu from "../../graphql/menu/getMenu.gql";
-import { Menu } from "../../entities";
-
-onMounted(async () => {
-  const { getMenu } = await runQuery(GetMenu);
-  menusStorage.setMenus(getMenu as unknown as [Menu]);
-});
-</script>

debe1ba238621d933ef7c96b784b2d31e2892db0
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2023-03-17 11:28:12 -0300

    🌐 Remoção dos textos de warning do menu para 'Campo necessário'

--- locales/br/warning.ts
+++ locales/br/warning.ts
@@ -2,14 +2,5 @@ export default {
   ramalName: "Por favor digite um nome",
   ramalSector: "Por favor digite um setor",
   ramalNumber: "Por favor digite um ramal",
-  menuWarning: {
-    complement: "Por favor digite um complemento",
-    rice: "Por favor digite um arroz",
-    salad: "Por favor digite uma salada",
-    protein: "Por favor digite uma proteína",
-    soup: "Por favor digite uma sopa",
-    day: "Por favor digite um dia",
-    week: "Por favor digite uma semana",
-    dessert: "Por favor digite uma sobremesa",
-  },
+  requiredField: "Campo necessário",
 };

ee9913f948344a91fd15f655068d2593775ea5f2
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2023-03-24 10:24:09 -0300

    💡 Adição comentários para seções a serem feitas

--- src/components/lib.ts
+++ src/components/lib.ts
@@ -1,6 +1,7 @@
 // lib local temporária para não poluir o código
 
 export const rhList = [
+  // TODO descomentar ao ir criando as páginas necessárias
   // {
   //   id: "1",
   //   img: "speaker_notes",
@@ -25,13 +26,13 @@ export const rhList = [
   //   label: "titles.Hr.ContactWithDirector",
   //   subLabel: "Digite uma mensagem",
   // },
-  {
-    id: "5",
-    img: "menu_book",
-    label: "titles.Hr.Menu",
-    subLabel: "Restaurante",
-    link: "/menu",
-  },
+  // {
+  //   id: "5",
+  //   img: "menu_book",
+  //   label: "titles.Hr.Menu",
+  //   subLabel: "Restaurante",
+  //   link: "/menu",
+  // },
   {
     id: "6",
     img: "phone",
@@ -39,7 +40,8 @@ export const rhList = [
     subLabel: "Listagem",
     link: "/ramais",
   },
-  // {
+  // TODO: descomentar ao fazer a página de agenda
+  //{
   //   id: "7",
   //   img: "calendar_month",
   //   label: "titles.Hr.Agenda",
@@ -68,39 +70,39 @@ export const gestaoList = [
     subLabel: "titles.DescriptionManagement.DataSulPrototype",
     link: "http://10.0.0.11:34080/totvs-login/loginForm",
   },
-  {
-    id: "4",
-    img: "/public/icon/codiIncon.png",
-    label: "titles.Management.StockQuery",
-    subLabel: "titles.DescriptionManagement.DataSulPrototype",
-    link: "http://10.10.0.3:8080/inventory",
-  },
-  {
-    id: "5",
-    img: "/public/icon/datasulIcon.png",
-    label: "titles.Management.BoaIdeia",
-    subLabel: "titles.DescriptionManagement.BoaIdeia",
-    link: "http://10.10.0.9/boaIdeia",
-  },
-  {
-    id: "6",
-    img: "rate_review",
-    label: "titles.Management.Reports",
-    subLabel: "titles.DescriptionManagement.Reports",
-    link: "",
-  },
-  {
-    id: "7",
-    img: "settings",
-    label: "titles.Management.ProcessMapping",
-    subLabel: "titles.DescriptionManagement.Mapping",
-    link: "http://10.10.0.6:3022/mapeamento/index/Index_arquivos/frame.html",
-  },
-  {
-    id: "8",
-    img: "settings",
-    label: "titles.Management.FtWeb",
-    subLabel: "titles.DescriptionManagement.FT",
-    link: "http://10.10.0.29:8180/ftweb/pages/login.jsp",
-  },
+  // {
+  //   id: "4",
+  //   img: "/public/icon/codiIncon.png",
+  //   label: "titles.Management.StockQuery",
+  //   subLabel: "titles.DescriptionManagement.DataSulPrototype",
+  //   link: "http://10.10.0.3:8080/inventory",
+  // },
+  // {
+  //   id: "5",
+  //   img: "/public/icon/datasulIcon.png",
+  //   label: "titles.Management.BoaIdeia",
+  //   subLabel: "titles.DescriptionManagement.BoaIdeia",
+  //   link: "http://10.10.0.9/boaIdeia",
+  // },
+  // {
+  //   id: "6",
+  //   img: "rate_review",
+  //   label: "titles.Management.Reports",
+  //   subLabel: "titles.DescriptionManagement.Reports",
+  //   link: "",
+  // },
+  // {
+  //   id: "7",
+  //   img: "settings",
+  //   label: "titles.Management.ProcessMapping",
+  //   subLabel: "titles.DescriptionManagement.Mapping",
+  //   link: "http://10.10.0.6:3022/mapeamento/index/Index_arquivos/frame.html",
+  // },
+  // {
+  //   id: "8",
+  //   img: "settings",
+  //   label: "titles.Management.FtWeb",
+  //   subLabel: "titles.DescriptionManagement.FT",
+  //   link: "http://10.10.0.29:8180/ftweb/pages/login.jsp",
+  // },
 ];

c6ab14fd753aa00735c971d0607ad5160bc2b19c
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2023-03-30 14:51:56 -0300

    💄 Alterações no Sistema de RH

--- src/components/SistemaRh/index.vue
+++ src/components/SistemaRh/index.vue
@@ -2,27 +2,41 @@
   <div>
     <q-card class="q-mx-lg row justify-center shadow-7">
       <q-card-section class="q-my-md">
-        <span class="text-uppercase row font text-bold text-black">
+        <span class="text-uppercase row text-h5 text-bold text-black">
           {{ $t("titles.HrSystem") }}
-          <q-icon color="primary" class="q-ml-sm" name="engineering" size="2rem" />
+          <q-icon
+            color="primary"
+            class="q-ml-sm"
+            name="engineering"
+            size="2rem"
+          />
         </span>
-        <q-separator size="0.5rem" color="primary" class="bar-style" />
+        <q-separator
+          size="0.5rem"
+          color="primary"
+          class="border-radius-inherit"
+        />
       </q-card-section>
       <q-card-section class="row col-12 justify-around">
         <q-item
           v-for="icon in rhList"
           :key="icon.id"
-          class="column col-4 q-my-md items-center"
+          class="column col-4 q-my-md items-center color-grey"
           clickable
           :href="icon.link"
         >
           <q-item-section wrap>
             <q-avatar size="7rem">
-              <q-icon class="border icon" :name="icon.icon" />
+              <q-icon
+                class="border icon border-radius-inherit q-pa-md"
+                color="grey"
+                size="4rem"
+                :name="icon.icon"
+              />
             </q-avatar>
           </q-item-section>
 
-          <q-item-section>
+          <q-item-section class="q-mt-md">
             <q-item-label class="text-red text-weight-bolder text-no-wrap">
               {{ $t(icon.label) }}
             </q-item-label>
@@ -43,29 +57,12 @@ import { rhList } from "../lib";
 <style scoped>
 .border {
   border: 8px solid;
-  border-radius: 50%;
-  padding: 0.3rem;
   border-color: rgb(164, 164, 164);
-  width: 70%;
-  height: 70%;
 }
-
-.icon {
-  color: grey;
-}
-
 .icon:hover {
   color: rgb(229, 57, 53);
 }
-
 .border:hover {
   border-color: rgb(229, 57, 53);
-}
-.bar-style {
-  border-radius: 10px;
-}
-
-.font {
-  font-size: 1.6rem;
 }
 </style>

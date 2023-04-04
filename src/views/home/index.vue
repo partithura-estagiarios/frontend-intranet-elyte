ec4790995198678bd4b2a802831a1728d8e21be5
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2023-04-04 11:35:55 -0300

    🥚 Começo alteração na view para pegar dados sobre o user

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,6 +2,13 @@
   <MainLayout />
 </template>
 
+<script lang="ts" setup>
+const teste = userStorage.getUser;
+if (teste.username == "") {
+  loga("u");
+}
+</script>
+
 <style scoped>
 .headerBar {
   background-color: brown;

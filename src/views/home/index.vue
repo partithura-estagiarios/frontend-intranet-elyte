1c6c2f7e193cfa7a63cb94076561d6de901e60ce
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2023-04-05 13:54:18 -0300

    🔥 Remoção script da index da view Home

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,13 +2,6 @@
   <MainLayout />
 </template>
 
-<script lang="ts" setup>
-const teste = userStorage.getUser;
-if (teste.username == "") {
-  loga("u");
-}
-</script>
-
 <style scoped>
 .headerBar {
   background-color: brown;

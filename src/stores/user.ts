6310ebb7a1fb70f7077590865ff0b790c6997d87
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2023-04-11 10:52:26 -0300

    🏗️ Alterações na store para persistir dados dos usuários

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -21,18 +21,21 @@ export const useUserStore = defineStore({
       return this.user;
     },
     isLoggedIn(): boolean {
-      if (this.getToken) {
-        return true;
-      }
-      return false;
+      return !!this.getToken;
     },
   },
   actions: {
     setUser(value: UserStorage) {
       Object.assign(this.$state.user, value);
+      const userData = `{"name": "${value.name}", "email":"${value.email}", "id":"${value.id}", "token":"${value.token}"}`;
+      localStorage.setItem("userData", userData);
     },
     logout() {
       clearStorage();
+      this.$state.user.name = "";
+      this.$state.user.email = "";
+      this.$state.user.token = "";
+      this.$state.user.id = "";
     },
   },
   persist: true,

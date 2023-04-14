bf1736dbd2efdc15e39c209c9786ec025f4b053e
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2023-04-14 08:45:27 -0300

    🎨 Inicialização da store de menus na index da view de menus

--- src/views/menu/Index.vue
+++ src/views/menu/Index.vue
@@ -1,3 +1,13 @@
 <template>
   <MenuTable />
 </template>
+
+<script setup lang="ts">
+import GetMenu from "../../graphql/menu/GetMenu.gql";
+import { Menu } from "../../entities/index";
+
+onMounted(async () => {
+  const { getMenu } = await runMutation(GetMenu, {});
+  menusStorage.setMenus(getMenu as unknown as Menu);
+});
+</script>

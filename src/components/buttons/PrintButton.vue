09d230ad6ecd9685837cc4d2ca58d32bde713702
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2023-04-19 09:03:06 -0300

    ✨ Criação componente PrintButton

---
+++ src/components/buttons/PrintButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-btn
+    class="row q-mt-md q-mr-md hide-print"
+    color="secondary"
+    :label="$t('action.print.index')"
+    size="1.2rem"
+    @click="printWindow"
+  />
+</template>
+
+<script lang="ts" setup>
+const printWindow = function () {
+  window.print();
+};
+</script>
+
+<style>
+@page {
+  margin: 0cm;
+  padding: 0cm;
+  size: landscape;
+}
+
+@media print {
+  .hide-print {
+    visibility: hidden;
+  }
+}
+
+* {
+  -webkit-print-color-adjust: exact !important;
+  print-color-adjust: exact !important;
+}
+</style>

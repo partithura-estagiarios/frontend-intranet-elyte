e0a980e4273f7da94c5b6893ef1126be4c7fdd34
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2023-04-25 13:55:05 -0300

    ⚡️ Criação componente dinamico para confirmar deleção

---
+++ src/components/ConfirmDelete.vue
@@ -0,0 +1,27 @@
+<template>
+  <DynamicDialog v-bind="$props" :title="$t('action.confirm.index')">
+    <span class="text-subtitle1">{{ text }}</span>
+    <q-card-actions align="right">
+      <q-btn
+        flat
+        :label="$t('action.confirm.index')"
+        color="primary"
+        @click="emit('confirm', id)"
+      />
+      <q-btn
+        flat
+        :label="$t('action.cancel.index')"
+        color="primary"
+        @click="emit('cancel')"
+      />
+    </q-card-actions>
+  </DynamicDialog>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(["cancel", "confirm"]);
+defineProps({
+  text: String,
+  id: String,
+});
+</script>

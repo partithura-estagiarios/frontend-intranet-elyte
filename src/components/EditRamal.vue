e5db950fe92af96fe787e5a4fdefe7efaa2b1420
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2023-04-26 13:39:21 -0300

    ✨ Criação componente EditRamal.vue

---
+++ src/components/EditRamal.vue
@@ -0,0 +1,49 @@
+<template>
+  <DynamicDialog :title="$t('action.editRamal.index')" v-bind="item">
+    <q-form>
+      <q-input
+        v-model="item.ramal_user"
+        :label="$t('text.name')"
+        stack-label
+        lazy-rules
+        :rules="[(val) => (val && val.length > 0) || $t('warning.ramalName')]"
+      />
+      <q-input
+        v-model="item.sector_user"
+        :label="$t('text.sector')"
+        stack-label
+        lazy-rules
+        :rules="[(val) => (val && val.length > 0) || $t('warning.ramalSector')]"
+      />
+      <q-input
+        v-model="item.ramal_number"
+        :label="$t('text.number')"
+        stack-label
+        lazy-rules
+        :rules="[(val) => (val && val.length > 0) || $t('warning.ramalNumber')]"
+      />
+      <q-card-actions align="right">
+        <q-btn
+          flat
+          :label="$t('action.confirm.index')"
+          color="primary"
+          @click="emit('confirm', item)"
+        />
+        <q-btn
+          flat
+          :label="$t('action.cancel.index')"
+          color="primary"
+          @click="emit('cancel')"
+        />
+      </q-card-actions>
+    </q-form>
+  </DynamicDialog>
+</template>
+
+<script lang="ts" setup>
+const emit = defineEmits(["cancel", "confirm"]);
+
+defineProps({
+  item: Object,
+});
+</script>

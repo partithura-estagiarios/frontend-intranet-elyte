17d12f2d2132215aaade71ab6e4dab88aaac1369
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2023-04-28 14:00:24 -0300

    🔥 Ajuste função retornando valor proprio nao mais variável

--- src/helpers/date.ts
+++ src/helpers/date.ts
@@ -1,13 +1,11 @@
 export function generateDate(dateString: string): Date {
-  const date = new Date(`${dateString}:00:00:00`);
-  return date;
+  return new Date(`${dateString}:00:00:00`);
 }
 
 export function generateDateForm(dateMiliseconds: number) {
-  const date = new Date(dateMiliseconds).toLocaleDateString("pt-BR", {
+  return new Date(dateMiliseconds).toLocaleDateString("pt-BR", {
     day: "numeric",
     month: "numeric",
     year: "numeric",
   });
-  return date;
 }

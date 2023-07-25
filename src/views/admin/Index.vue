cc90e8f3a7fb46773e743da2c502b4d5be7a97ad
vitorscorrea <vitornbls2000@gmail.com>
2023-07-25 14:49:42 -0300

    ✨ Criação tela de admin

---
+++ src/views/admin/Index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+// FIXME: Será funcional quando existir a tela de login PR #31
+// onMounted(() => {
+//     if(!userStorage.isLoggedIn){
+//         router.push('login')
+//     }
+// })
+</script>
+
+<template>
+  <MainLayout />
+</template>

12db179d5d8dc92af6aec6afcd5d05dfb512c271
Scarlett <scarlettxoliveira@gmail.com>
2023-08-16 10:36:39 -0300

    🩹 Ajuste na tradução

--- locales/en/warning.ts
+++ locales/en/warning.ts
@@ -2,7 +2,7 @@ export default {
   ramalName: "Please enter a name",
   ramalSector: "Please enter a sector",
   ramalNumber: "Please enter a number",
-  email: "Email inválido",
+  email: "Invalid email",
   pwd: "Invalid password, minimum 8 characteres",
   username: "Invalid username, minimum 2 characteres",
   lowercase: "Must contain a lowercase letter",

8b86aef1fec58da9581cf178d203f1aba7f0c36c
Scarlett <scarlettxoliveira@gmail.com>
2023-08-17 11:22:21 -0300

    🦺 Validação para o email, username e password

---
+++ src/validation.ts
@@ -0,0 +1,24 @@
+import * as yup from "yup";
+
+export const validationSchema = yup.object({
+  username: yup
+    .string()
+    .required(t("warning.requiredField"))
+    .min(2, t("warning.username")),
+  email: yup
+    .string()
+    .required(t("warning.requiredField"))
+    .email(t("warning.email")),
+  password: yup
+    .string()
+    .required(t("warning.requiredField"))
+    .min(8, t("warning.pwd"))
+    .matches(/[a-z]/, t("warning.lowercase"))
+    .matches(/[A-Z]/, t("warning.uppercase"))
+    .matches(/\d/, t("warning.containDigit"))
+    .matches(/[!@#$%^&*(),.?":{}|<>]/, t("warning.containSpecialChar")),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password")], t("warning.matchingPwd"))
+    .required(t("warning.requiredField")),
+});

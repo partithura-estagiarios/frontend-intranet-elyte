c03a1e6277e7c0316aeab8428bfc96aef63d4c50
Scarlett <scarlettxoliveira@gmail.com>
2023-10-03 13:28:53 -0300

    🩹 Faltava um p em support

--- locales/en/label.ts
+++ locales/en/label.ts
@@ -35,7 +35,7 @@ export default {
   },
   room: "Room",
   support: {
-    index: "Suports Material",
+    index: "Support Materials",
     computer: "Computer",
     water: "Water",
     coffee: "Coffee",

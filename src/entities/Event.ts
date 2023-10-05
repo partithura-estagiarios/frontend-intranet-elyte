9e686dff69fcd23145884c6cb71d49238e02f3f7
lauragabriele <lauragabriele2015@gmail.com>
2023-10-05 09:37:34 -0300

    🐛 remoção de uma interface

--- src/entities/Event.ts
+++ src/entities/Event.ts
@@ -19,15 +19,3 @@ export interface Room {
   name: string;
   color: string;
 }
-
-export interface EventTimes {
-  hour: number;
-  initial: {
-    value: number;
-    available: boolean;
-  };
-  half: {
-    value: number;
-    available: boolean;
-  };
-}

93cd927cbdcae6d5111e6b75dd2f76d8f505328f
Scarlett <scarlettxoliveira@gmail.com>
2023-10-23 09:55:56 -0300

    🎨 Componente de um botão para navegação

---
+++ src/components/buttons/NavigationButton.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps({
+  icon: {
+    type: String,
+    default: "",
+  },
+  navigationStep: {
+    type: String,
+    default: "",
+  },
+  label: {
+    type: String,
+    defauly: "",
+  },
+});
+</script>
+
+<template>
+  <q-btn no-caps flat class="rounded" dense :icon="icon" :label="label" />
+</template>

1b301a4d55fb1dd0267040e123b24d84052e39c0
lauragabriele <lauragabriele2015@gmail.com>
2023-10-27 09:07:20 -0300

    💄 ajuste no estilo dos modais

--- src/components/CreateMenu/index.vue
+++ src/components/CreateMenu/index.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import * as yup from "yup";
-import { Field, Form } from "vee-validate";
+import { Form } from "vee-validate";
 import AddMenu from "../../graphql/menu/AddMenu.gql";
 import EditMenu from "../../graphql/menu/EditMenu.gql";
 import GetMenu from "../../graphql/menu/GetMenu.gql";
+import { menuSchema } from "../../validation";
 import { Menu } from "../../entities";
 import { Ref } from "vue";
 import { onMounted, defineEmits } from "vue";
+import { emit } from "process";
 
 onMounted(() => {
   getMenu();
@@ -85,52 +87,19 @@ const tableColumns = [
   },
 ];
 const action: Ref<"add" | "edit" | null> = ref(null);
-const opt = {
-  edit: () => editMenu(),
-  add: () => addMenu(),
-};
 
-const schema = yup.object({
-  date: makeRuleOfString(),
-  salad: makeRuleOfString(),
-  rice: makeRuleOfString(),
-  protein: makeRuleOfString(),
-  complement: makeRuleOfString(),
-  soup: makeRuleOfString(),
-  dessert: makeRuleOfString(),
-});
-
-function makeRuleOfString(message = "warning.requiredField") {
-  return yup.string().required(t(message));
-}
-
-async function changeMutation() {
+async function chooseMutation(data) {
   if (action.value !== null) {
-    if (validateForm()) {
-      opt[action.value]();
+    if (menuSchema) {
+      opt[action.value](data);
     }
   }
 }
 
-function validateForm() {
-  const requiredFields = [
-    "date",
-    "salad",
-    "rice",
-    "protein",
-    "complement",
-    "soup",
-    "dessert",
-  ];
-
-  for (const field of requiredFields) {
-    if (!form[field]) {
-      return false;
-    }
-  }
-
-  return true;
-}
+const opt = {
+  edit: (data) => editMenu(data),
+  add: (data) => addMenu(data),
+};
 
 async function getMenu() {
   const { getMenu: rawData } = await runQuery(GetMenu);
@@ -149,7 +118,8 @@ async function getMenu() {
   }
 }
 
-async function editMenu() {
+async function editMenu(data) {
+  data.date = form.date;
   try {
     const isDuplicateDate = menus.value.some((menu) => menu.date === form.date);
     if (isDuplicateDate) {
@@ -157,25 +127,26 @@ async function editMenu() {
     }
     const response = await runMutation(EditMenu, {
       id: item.id,
-      data: form,
+      data,
     });
     if (response && response.editMenu) {
       positiveNotify(t("notifications.success.editMenu"));
       await getMenu();
       closeAddModal();
-      emits("reload");
+      router.go(0);
     }
   } catch {
     negativeNotify(t("notifications.fail.createMenu"));
   }
 }
 
-async function addMenu() {
+async function addMenu(data) {
+  data.date = form.date;
   try {
-    await runMutation(AddMenu, { data: form });
+    await runMutation(AddMenu, { data });
     positiveNotify("notifications.success.createMenu");
     closeAddModal();
-    emits("reload");
+    router.go(0);
   } catch {
     negativeNotify(t("notifications.fail.createMenu"));
   }
@@ -196,8 +167,13 @@ function openModal(modal: "add" | "edit", id?: string) {
 
 function closeAddModal() {
   showAddModal.value = false;
-
   action.value = null;
+}
+
+function triggerwarning() {
+  if (!form.date) {
+    negativeNotify(t("warning.dateFieldEmpty"));
+  }
 }
 </script>
 <template>
@@ -215,7 +191,7 @@ function closeAddModal() {
           icon="home"
           color="primary"
           size="1.2rem"
-          class="justify-start row q-ml-md"
+          class="justify-start row q-ml-xl"
           @click="router.back"
         />
       </div>
@@ -264,106 +240,113 @@ function closeAddModal() {
   </div>
   <DynamicDialog
     v-model="showAddModal"
-    @confirm="changeMutation()"
     @cancel="showAddModal = false"
+    :title="$t('titles.Hr.Menu')"
+    class="q-ma-xl"
+    hide-controls
   >
-    <q-card class="q-card">
-      <q-card-section>
-        <Form :validation-schema="schema">
-          <Field name="date" v-slot="item">
-            <q-input
-              v-model="form.date"
-              :label="$t('text.day')"
-              filled
-              @click="showCalendar = true"
-            />
-            <div style="position: relative">
-              <q-date
-                v-model="form.date"
-                v-if="showCalendar"
-                :label="$t('text.day')"
-                bg-color="grey-3"
-                class="text-black calendar"
-                @click="showCalendar = false"
-              />
-            </div>
-            <span v-if="item.errorMessage" class="text-red q-mb-xl">
-              {{ parseErrorMessage(item.errorMessage) }}
-            </span>
-          </Field>
+    <Form
+      :validation-schema="menuSchema"
+      class="row q-gutter-sm"
+      @submit="chooseMutation"
+    >
+      <q-input
+        v-model="form.date"
+        :label="$t('text.day')"
+        filled
+        @click="showCalendar = true"
+        class="col-12"
+      />
+      <q-date
+        flat
+        minimal
+        v-model="form.date"
+        v-if="showCalendar"
+        :label="$t('text.day')"
+        bg-color="grey-3"
+        class="text-black calendar"
+        @click="showCalendar = false"
+      />
+      <StandardInput
+        field-name="salad"
+        field-color="white"
+        class="col-12"
+        :field-label="$t('text.menu.salad')"
+        borderless
+        icon-name="eco"
+        icon-class="fit q-px-xs"
+        icon-size="md"
+        icon-color="primary"
+      />
 
-          <Field name="salad" v-slot="item">
-            <q-input
-              v-model="form.salad"
-              v-bind="item.field"
-              :label="$t('text.menu.salad')"
-              filled
-            />
-            <span v-if="item.errorMessage" class="text-red q-mb-xl">
-              {{ parseErrorMessage(item.errorMessage) }}
-            </span>
-          </Field>
-          <Field name="rice" v-slot="item">
-            <q-input
-              v-model="form.rice"
-              v-bind="item.field"
-              :label="$t('text.menu.rice')"
-              filled
-            />
-            <span v-if="item.errorMessage" class="text-red q-mb-xl">
-              {{ parseErrorMessage(item.errorMessage) }}
-            </span>
-          </Field>
-          <Field name="protein" v-slot="item">
-            <q-input
-              v-model="form.protein"
-              v-bind="item.field"
-              :label="$t('text.menu.protein')"
-              filled
-            />
-            <span v-if="item.errorMessage" class="text-red q-mb-xl">
-              {{ parseErrorMessage(item.errorMessage) }}
-            </span>
-          </Field>
+      <StandardInput
+        field-name="rice"
+        field-color="white"
+        class="col-12"
+        :field-label="$t('text.menu.rice')"
+        borderless
+        icon-name="mdi-rice"
+        icon-class="fit q-px-xs"
+        icon-size="md"
+        icon-color="primary"
+      />
 
-          <Field name="complement" v-slot="item">
-            <q-input
-              v-model="form.complement"
-              v-bind="item.field"
-              :label="$t('text.menu.complement')"
-              filled
-            />
-            <span v-if="item.errorMessage" class="text-red q-mb-xl">
-              {{ parseErrorMessage(item.errorMessage) }}
-            </span>
-          </Field>
+      <StandardInput
+        field-name="protein"
+        field-color="white"
+        class="col-12"
+        :field-label="$t('text.menu.protein')"
+        borderless
+        icon-name="mdi-food-drumstick"
+        icon-class="fit q-px-xs"
+        icon-size="md"
+        icon-color="primary"
+      />
 
-          <Field name="soup" v-slot="item">
-            <q-input
-              v-model="form.soup"
-              v-bind="item.field"
-              :label="$t('text.menu.soup')"
-              filled
-            />
-            <span v-if="item.errorMessage" class="text-red q-mb-xl">
-              {{ parseErrorMessage(item.errorMessage) }}
-            </span>
-          </Field>
+      <StandardInput
+        field-name="complement"
+        field-color="white"
+        class="col-12"
+        :field-label="$t('text.menu.complement')"
+        borderless
+        icon-name="mdi-noodles"
+        icon-class="fit q-px-xs"
+        icon-size="md"
+        icon-color="primary"
+      />
 
-          <Field name="dessert" v-slot="item">
-            <q-input
-              v-model="form.dessert"
-              v-bind="item.field"
-              :label="$t('text.menu.dessert')"
-              filled
-            />
-            <span v-if="item.errorMessage" class="text-red q-mb-xl">
-              {{ parseErrorMessage(item.errorMessage) }}
-            </span>
-          </Field>
-        </Form>
-      </q-card-section>
-    </q-card>
+      <StandardInput
+        field-name="soup"
+        field-color="white"
+        class="col-12"
+        :field-label="$t('text.menu.soup')"
+        borderless
+        icon-name="soup_kitchen"
+        icon-class="fit q-px-xs"
+        icon-size="md"
+        icon-color="primary"
+      />
+
+      <StandardInput
+        field-name="dessert"
+        field-color="white"
+        class="col-12"
+        :field-label="$t('text.menu.dessert')"
+        borderless
+        icon-name="mdi-ice-cream"
+        icon-class="fit q-px-xs"
+        icon-size="md"
+        icon-color="primary"
+      />
+      <Button class="bg-transparent no-padding">
+        <q-btn
+          flat
+          :label="$t('action.confirm.index')"
+          color="primary"
+          @click="triggerwarning"
+        />
+      </Button>
+    </Form>
   </DynamicDialog>
 </template>
 
@@ -371,14 +354,20 @@ function closeAddModal() {
 .font {
   font-size: 2rem;
 }
-
-.bar-style {
-  border-radius: 10px;
+.btn {
+  display: flex;
+  justify-content: flex-end;
 }
+
 .calendar {
+  justify-content: center;
+  display: flex;
   position: absolute;
-  top: 100%;
-  left: 0;
   z-index: 9999;
+  left: 0;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+  top: 60px;
 }
 </style>
